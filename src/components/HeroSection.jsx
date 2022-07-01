60092860e138eec8237b07aafb5c3d6046aab81f
haeun-jo <gkdmsdl0727@gmail.com>
2022-07-01 17:24:39 +0900

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <div className="flex flex-col justify-center items-center py-20">
+      <div className="text-center">
+        <h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 font-semibold">
+          Hi, This is HaEun
+        </h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
+          earum dolorum reprehenderit aut corrupti veniam expedita eligendi,
+          suscipit dicta quasi quae autem saepe laudantium consequuntur quas sit
+          voluptas nulla maiores!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
